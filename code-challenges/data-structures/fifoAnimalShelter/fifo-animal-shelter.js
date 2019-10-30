bdb10957e5b49b4a36a224d95ab247978dcc357f
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-10-29 23:02:22 -0700

    animal shelter not finished

---
+++ code-challenges/data-structures/fifoAnimalShelter/fifo-animal-shelter.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+class AnimalShelter {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
+  }
+
+  enqueue(animal) {
+    if (animal.type === 'dog')
+      this.stack1.push(animal);
+    if (animal.type === 'cat')
+      this.stack1.push(animal);
+  }
+
+  dequeue() {
+    if (this.stack2.length === 0) {
+      if (this.stack1.length === 0) {
+        return 'empty shelter';
+      }
+      while (this.stack1.length > 0) {
+        let poppedAnimal = this.stack1.pop();
+        this.stack2.push(poppedAnimal);
+      }
+      console.log('stack 2 :', this.stack2);
+      for (let i = 0; i <= this.stack2.length; i++) {
+        if (this.stack2[i].type === 'dog') {
+          console.log('got it');
+        }
+        else {
+          return null;
+        }
+      }
+    }
+  }
+}
+
+
+
+const peanut = {
+  type: 'dog',
+}
+const jenny = {
+  type: 'cat',
+}
+
+
+const shelter = new AnimalShelter();
+
+shelter.enqueue(peanut);
+shelter.enqueue(jenny);
+// shelter.dequeue('dog');
+// console.log(shelter.stack1);
+shelter.dequeue();
+// console.log(shelter.stack2);
+// console.log(shelter.dequeue('dog'));
+
+
