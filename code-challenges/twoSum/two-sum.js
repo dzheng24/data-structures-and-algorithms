1b52ef20b2d5dae88f87087e511da4cdc17824ae
David Zheng <zhengdav@msu.edu>
2019-12-24 23:41:53 -0800

    test partially passing

--- code-challenges/twoSum/two-sum.js
+++ code-challenges/twoSum/two-sum.js
@@ -1,5 +1,20 @@
 'use strict';
 
 function twoSum (nums, target) {
-    
+  const previousValues = {};
+  for (let i = 0; i < nums.length; i++) {
+    const currentValue = nums[i];
+    const neededValue = target - currentValue;
+    const index2 = previousValues[neededValue];
+    if (index2) {
+      return [index2, i];
+    } else {
+      previousValues[currentValue] = i;
+    }
+  }
 }
+
+console.log(twoSum([1,3,4,5], 4));
+
+module.exports = twoSum;
+
