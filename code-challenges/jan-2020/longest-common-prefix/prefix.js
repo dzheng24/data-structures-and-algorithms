d98c5b08f62304a42f5508e19c7640f41b4302e8
David Zheng <zhengdav@msu.edu>
2020-01-11 14:27:24 -0800

    test passes, but not on leet

--- code-challenges/jan-2020/longest-common-prefix/prefix.js
+++ code-challenges/jan-2020/longest-common-prefix/prefix.js
@@ -1,8 +1,20 @@
 'use strict';
 
-function longestCommonPrefix (string) {
-
+function longestCommonPrefix (strings) {
+  let prefix = '';
+  for (let i = 0; i < strings[0].length; i++) {
+    const character = strings[0][i]
+    for (let j = 0; j < strings.length; j++) {
+      if (strings[j][i] !== character) {
+        return prefix;
+      }
+    }
+    prefix = prefix + character;
+  }
+  return prefix;
 }
+
+console.log(longestCommonPrefix(['cars', 'gam', 'sarrent']));
 
 module.exports = {
   longestCommonPrefix
