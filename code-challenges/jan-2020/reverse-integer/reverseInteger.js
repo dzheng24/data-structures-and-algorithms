762bd892a7125897eb6841b42234bf7b75860551
David Zheng <zhengdav@msu.edu>
2020-01-10 17:54:43 -0800

    test passes, but not on leet code

--- code-challenges/jan-2020/reverse-integer/reverseInteger.js
+++ code-challenges/jan-2020/reverse-integer/reverseInteger.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function reverse(num) {
-
+  let multiplier = 1;
+  if (num < 0) {
+    multiplier = -1;
+    num = Math.abs(num);
+  }
+  let reversed = 0;
+  while (num > 0) {
+    reversed = reversed * 10 + (num % 10);
+    num = parseInt(num / 10);
+  }
+  return reversed * multiplier;
 }
+
+console.log(reverse(1534236469));
 
 module.exports = {
   reverse
