251d1ea00812527f389b672b969e0c3dd1e3ee77
David Zheng <zhengdav@msu.edu>
2020-01-12 09:42:31 -0800

    wrote in stack class

--- code-challenges/jan-2020/valid-parentheses/valid.js
+++ code-challenges/jan-2020/valid-parentheses/valid.js
@@ -1,9 +1,62 @@
 'use strict';
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(value) {
+    let node = new Node(value);
+    node.next = this.top;
+    this.top = node;
+    return this;
+  }
+
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    let poppedValue = this.top.value;
+    this.top = this.top.next;
+    return poppedValue;
+  }
+
+  peek() {
+    if (!this.top) {
+      return null;
+    }
+    return this.top.value;
+  }
+
+  isEmpty() {
+    if (!this.top) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+}
+
 function isValid(string) {
 
 }
 
+let seahawks = new Stack();
+seahawks.push('russell');
+seahawks.push('marshawn');
+seahawks.push('dk');
+console.log(JSON.stringify(seahawks));
+
 module.exports = {
+  Node,
+  Stack,
   isValid
 }
