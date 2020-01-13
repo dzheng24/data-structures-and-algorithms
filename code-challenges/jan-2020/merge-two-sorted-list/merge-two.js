8953dffae618a182ea7551506607d5466781cbee
David Zheng <zhengdav@msu.edu>
2020-01-13 14:51:00 -0800

    passes on leet

--- code-challenges/jan-2020/merge-two-sorted-list/merge-two.js
+++ code-challenges/jan-2020/merge-two-sorted-list/merge-two.js
@@ -16,7 +16,26 @@ function ListNode(val) {
 }
 
 function mergeTwoLists(l1, l2) {
-
+  let test = new ListNode(-1);
+  let newHead = test;
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      test.next = l1;
+      l1 = l1.next;
+    }
+    else {
+      test.next = l2;
+      l2 = l2.next;
+    }
+    test = test.next;
+  }
+  if (l1 !== null) {
+    test.next = l1;
+  }
+  else {
+    test.next = l2;
+  }
+  return newHead.next;
 }
 
 let list1 = new ListNode(1);
