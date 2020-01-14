be3c28a1e546401cfebd6c02a4db3474e5894be0
David Zheng <zhengdav@msu.edu>
2020-01-14 12:17:12 -0800

    passes on leet

--- code-challenges/jan-2020/move-zeros/move.js
+++ code-challenges/jan-2020/move-zeros/move.js
@@ -5,8 +5,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-
+  let index = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[index++] = nums[i];
+    }
+  }
+  for (let i = index; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  console.log(nums);
 };
+
+
+moveZeroes([0,1,0,3,12]);
 
 module.exports = {
   moveZeroes
