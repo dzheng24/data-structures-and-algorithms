3d052fc1eb24c8165d4134c4b75400e135ff067f
David Zheng <zhengdav@msu.edu>
2020-01-15 13:09:55 -0800

    passes on leet

--- code-challenges/jan-2020/remove-duplicates-from-sorted-array/remove.js
+++ code-challenges/jan-2020/remove-duplicates-from-sorted-array/remove.js
@@ -5,8 +5,16 @@
  * @return {number}
  */
 function removeDuplicates(nums) {
-
+  let index = 1;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[index++] = nums[i + 1];
+    }
+  }
+  return index;
 }
+
+console.log(removeDuplicates([1,1,1,2,2,2]));
 
 module.exports = {
   removeDuplicates
