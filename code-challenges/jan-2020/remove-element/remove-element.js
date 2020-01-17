69bf2e4a13f45f79ace2cdd4938521a3a572935b
David Zheng <zhengdav@msu.edu>
2020-01-16 18:30:01 -0800

    passes on leet

--- code-challenges/jan-2020/remove-element/remove-element.js
+++ code-challenges/jan-2020/remove-element/remove-element.js
@@ -6,8 +6,19 @@
  * @return {number}
  */
 function removeElement(nums, val) {
-
+  let counter = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      counter++;
+    }
+    else {
+      nums[i] = nums[i - counter]
+    }
+  }
+  return nums.length - counter;
 }
+
+console.log(removeElement([3,2,2,3], 3))
 
 module.exports = {
   removeElement
