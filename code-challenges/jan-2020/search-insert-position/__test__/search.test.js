46f4dbd6968765dcad592142476e4c81abb64af0
David Zheng <zhengdav@msu.edu>
2020-01-19 13:55:25 -0800

    scaffold, wrote tests

---
+++ code-challenges/jan-2020/search-insert-position/__test__/search.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const searchInsert = require('../search').searchInsert;
+
+describe('testing the search insert function', () => {
+  it('can return the correct index when the number is in the array', () => {
+    expect(searchInsert([4, 6, 8, 9, 10], 9)).toEqual(3);
+    expect(searchInsert([2, 6, 8, 10, 11], 2)).toEqual(0);
+  })
+
+  it('can place the number in the correct index', () => {
+    expect(searchInsert([4, 6, 8, 9, 10], 7)).toEqual(2);
+    expect(searchInsert([4, 6, 8, 9, 10], 9)).toEqual(3);
+    expect(searchInsert([3, 8, 11, 88, 90], 2)).toEqual(0);
+  })
+})
---
+++ code-challenges/jan-2020/search-insert-position/search.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function searchInsert (nums, target) {
+
+}
+
+module.exports = {
+  searchInsert
+}
+
