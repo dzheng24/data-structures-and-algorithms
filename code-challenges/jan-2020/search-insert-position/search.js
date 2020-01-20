17e97335be87ef4cc66f6c42ca35a690d0afb6a4
David Zheng <zhengdav@msu.edu>
2020-01-19 16:35:08 -0800

    test passes

--- code-challenges/jan-2020/search-insert-position/search.js
+++ code-challenges/jan-2020/search-insert-position/search.js
@@ -6,8 +6,22 @@
  * @return {number}
  */
 function searchInsert (nums, target) {
-
+  let nextIndex;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      return i;
+    }
+    if (nums[i] < target && nums[i + 1] > target) {
+      nextIndex = i + 1;
+      return nextIndex;
+    }
+    if (nums[0] > target) {
+      return 0;
+    }
+  }
 }
+
+console.log(searchInsert([1,3,5,6], 0))
 
 module.exports = {
   searchInsert
