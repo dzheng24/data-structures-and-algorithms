a0daf0ca8ebd897e9a0b558d0dad8894f08a697b
David Zheng <zhengdav@msu.edu>
2020-01-21 13:46:53 -0800

    passes on leet

--- code-challenges/jan-2020/count-and-say/count.js
+++ code-challenges/jan-2020/count-and-say/count.js
@@ -1,12 +1,40 @@
+/* eslint-disable indent */
 'use strict';
 
 /**
  * @param {number} n
  * @return {string}
  */
-function countAndSay(n) {
 
+
+function countAndSay(n) {
+  if(n <= 0) return '-1';
+let result = '1';
+  for (let i = 1; i < n; i++) {
+    result = build(result);
+  }
+  return result;
 }
+
+function build(result) {
+  let builder = new Array();
+  let p = 0;
+  while (p < result.length) {
+    let val = result.charAt(p);
+    let count = 0;
+
+    while (p < result.length && result.charAt(p) === val) {
+      p++;
+      count++;
+    }
+    builder.push(count);
+    builder.push(val);
+  }
+  return builder.join('');
+}
+
+console.log(countAndSay(8));
+
 
 
 module.exports = {
