52e50a53f74035830e365f6bf3651b25a7911c3a
David Zheng <zhengdav@msu.edu>
2020-01-22 15:18:34 -0800

    removed console logs, updated readme

--- code-challenges/jan-2020/count-and-say/count.js
+++ code-challenges/jan-2020/count-and-say/count.js
@@ -1,4 +1,3 @@
-/* eslint-disable indent */
 'use strict';
 
 /**
@@ -11,36 +10,26 @@ function countAndSay(n) {
   if(n <= 0) return '-1';
   let result = '1';
   for (let i = 1; i < n; i++) {
-    console.log('i is ', i)
     result = build(result);
   }
-  console.log('final result is ', result)
   return result;
 }
 
-function build(result) {
-  console.log('the result currently is ', result)
+function build(lastResult) {
   let stringBuilder = new Array();
   let p = 0;
-  while (p < result.length) {
-    console.log('p is ', p + ' and result is ', result)
-    let val = result.charAt(p);
+  while (p < lastResult.length) {
+    let val = lastResult.charAt(p);
     let count = 0;
-
-    while (p < result.length && result.charAt(p) === val) {
+    while (p < lastResult.length && lastResult.charAt(p) === val) {
       p++;
       count++;
     }
     stringBuilder.push(count);
     stringBuilder.push(val);
   }
-  console.log('builder is ', stringBuilder)
   return stringBuilder.join('');
 }
-
-console.log(countAndSay(4));
-
-
 
 module.exports = {
   countAndSay
