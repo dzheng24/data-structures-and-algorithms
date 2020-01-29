c339af87c876024db3d0b0b52e7670e026f34375
David Zheng <zhengdav@msu.edu>
2020-01-28 21:08:16 -0800

    added cubic time solution

--- code-challenges/jan-2020/max-subarray/max.js
+++ code-challenges/jan-2020/max-subarray/max.js
@@ -4,9 +4,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
 
-};
+// runs in cubic time
+var maxSubArray = function(nums) {
+  const n = nums.length;
+  let maxSubArraySum = Number.MIN_VALUE;
+
+  for (let left = 0; left < n; left++) {
+    for (let right = left; right < n; right++) {
+      let windowSum = 0;
+      for (let k = left; k <= right; k++) {
+        windowSum += nums[k];
+      }
+      maxSubArraySum = Math.max(maxSubArraySum, windowSum)
+    }
+  }
+  return maxSubArraySum;
+}
 
 module.exports = {
   maxSubArray
