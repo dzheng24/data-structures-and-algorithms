10fe378e905b123b1c620bd80421ac79da908d50
David Zheng <zhengdav@msu.edu>
2020-02-02 15:09:17 -0800

    rewrote code, passes on leetcode

--- code-challenges/feb-2020/plus-one/plus.js
+++ code-challenges/feb-2020/plus-one/plus.js
@@ -5,21 +5,22 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-  const inputNumber = parseInt(digits.join(''));
-  const newNumber = inputNumber + 1;
-  const newStringNumberArray = newNumber.toString().split('');
-  const n = newStringNumberArray.length;
-  let resultArray = [];
-  for (let i = 0; i < n; i++) {
-    let popped = newStringNumberArray.pop();
-    popped = parseInt(popped);
-    resultArray.unshift(popped);
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    else {
+      digits[i] = 0
+    }
   }
-  return resultArray;
-};
+
+  digits.unshift(1);
+  return digits;
+}
 
 
-console.log(plusOne([2,3,4]));
+console.log(plusOne([9,0,9]));
 
 module.exports = {
   plusOne
