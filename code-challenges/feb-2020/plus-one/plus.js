d09405601705a9eb38a869b304db539e93dba157
David Zheng <zhengdav@msu.edu>
2020-02-02 11:23:06 -0800

    local test passes

--- code-challenges/feb-2020/plus-one/plus.js
+++ code-challenges/feb-2020/plus-one/plus.js
@@ -5,8 +5,21 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    
+  const inputNumber = parseInt(digits.join(''));
+  const newNumber = inputNumber + 1;
+  const newStringNumberArray = newNumber.toString().split('');
+  const n = newStringNumberArray.length;
+  let resultArray = [];
+  for (let i = 0; i < n; i++) {
+    let popped = newStringNumberArray.pop();
+    popped = parseInt(popped);
+    resultArray.unshift(popped);
+  }
+  return resultArray;
 };
+
+
+console.log(plusOne([2,3,4]));
 
 module.exports = {
   plusOne
