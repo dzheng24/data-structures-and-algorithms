d7235e89ea201fdaf1783f311d7e65ebfdbade32
David Zheng <zhengdav@msu.edu>
2020-02-01 23:10:25 -0800

    local test passes

--- code-challenges/jan-2020/length-of-last-word/word.js
+++ code-challenges/jan-2020/length-of-last-word/word.js
@@ -5,7 +5,17 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    
+  if (s === null) {
+    return null;
+  }
+  let length = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === ' ') {
+      break;
+    }
+    length++;
+  }
+  return length;
 };
 
 module.exports = {
