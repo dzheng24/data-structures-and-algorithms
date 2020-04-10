7703cf13ad99e93f80a9d6048363482467a8f4ab
David Zheng <zhengdav@msu.edu>
2020-04-10 11:33:54 -0700

    completed regex1 problems

--- warmup-challenges/apr-2020/4-9-regex1.test.js
+++ warmup-challenges/apr-2020/4-9-regex1.test.js
@@ -76,6 +76,8 @@ The expected output of "Hello, and have a wonderful day!" is ["and ", "have ", "
 
 const noPunctuation = str => {
   // Solution code here...
+  let finder = /\w+\s/gm;
+  return str.match(finder);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -92,6 +94,8 @@ For example, 'Welcome to Code 301!' will return 'W_lc_m_ t_ C_d_ 301!'.
 
 let hangman = (str) => {
   // Solution code here...
+  let finder = /[aeiou]/gi;
+  return str.replace(finder, '_');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -108,6 +112,8 @@ const seashells = 'She sells seashells by the seashore. The shells she sells are
 
 const findShells = (str) => {
   // Solution code here...
+  let finder = /sh?(eash)?(ells)/g;
+  return str.match(finder);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -182,7 +188,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia vel massa sed egestas. Nunc faucibus iaculis elit, a scelerisque enim condimentum sed. Aenean ac scelerisque sem, et pharetra diam.';
 
   test('It should only return words that are immediately followed by a space', () => {
@@ -196,7 +202,7 @@ xdescribe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   let startString = 'This is a regex challenge. We are trying to create a hangman phrase where all of the vowels are missing!';
 
   test('It should remove the vowels from the hangman string and replace them with underscores', () => {
@@ -209,7 +215,7 @@ xdescribe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return an array of instances of "sells", shells", and "seashells"', () => {
     expect(findShells(seashells)).toStrictEqual(['sells', 'seashells', 'shells', 'sells', 'seashells', 'sells', 'shells', 'sells', 'shells']);
     expect(findShells(seashells).length).toStrictEqual(9);
